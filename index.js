dec0b6fc428f9ee9884d0ce9cb4e241bd4ac7f4c
Carlos Binker <carlosbinker@gmail.com>
2025-06-01 14:58:51 -0300

    Consulta de todos los productos concluida

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import { argv } from "process"
-console.log(argv)
+
+//Primero que nada, voy a desestructurar el array argv. Los dos primeros elementos no me interesan porque son node y el script, así que sólo dejo las comas.
+
+let [, , method, resource] = argv;
+
+// console.log(method, resource);
+
+// npm run start GET products
+
+// Paso todo a minúscula para asegurararme una correcta comparación
+method = method.toLowerCase();
+resource = resource.toLowerCase()
+
+if (method == 'get' && resource == 'products') {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+}
+
+
+
