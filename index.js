97c0177435baf2d2de1bd10fcf964af6df027b90
Carlos Binker <carlosbinker@gmail.com>
2025-06-01 17:50:18 -0300

    Agregado de un producto mediante POST concluída

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { argv } from "process"
 
 //Primero que nada, voy a desestructurar el array argv. Los dos primeros elementos no me interesan porque son node y el script, así que sólo dejo las comas.
 
-let [, , method, resource] = argv;
+let [, , method, resource, title, price, category] = argv;
 
-// console.log(method, resource)
+console.log(method, resource, title, price, category)
 
 // Paso todo a minúscula para asegurararme una correcta comparación en las diferentes consignas
 method = method.toLowerCase();
@@ -29,6 +29,24 @@ if (method == "get" && resource == `products/${id}` && id != '') {
       .then((response) => response.json())
       .then((data) => console.log(data))
 }
+
+// 3° consigna - Creación de un nuevo producto. Sintaxis --> npm run start POST products <title> <price> <categoria>. Ejemplo: npm run start POST products T-Shirt-Rex 300 remeras
+
+if (method == "post" && resource == 'products') {
+    // Creo un objeto del tipo product con las llaves indicadas: title, price y category
+    const product = { title: title, price: parseInt(price), category: category};
+    fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+}
+
+
+
+
 
 // else {
 //     console.log("La consulta no cumple con la sintaxis solicitada");
